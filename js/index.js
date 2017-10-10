30782653a1cc86c55ae992d4b1011c80ab33d7f0
Skidi <speedy_rower@yahoo.com>
2017-10-10 11:11:46 +0300

    update javascript

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ function map() {
           disableDefaultUI:true,
           styles: [
           	{elementType: "geometry", stylers: [{color:"#000000"}]},
-          	{elementType: 'labels.text.stroke', stylers: [{color: '#f4eb42'}]},
+          	{elementType: 'labels.text.stroke', stylers: [{color: '#ffffff', Weight: 0.1}]},
           	{
               featureType: 'water',
               elementType: 'geometry',
@@ -32,12 +32,69 @@ function map() {
           map: map
         });
       
-}
+};
+
 function loadScript() {
   var script = document.createElement('script');
     script.type = 'text/javascript';
     script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&callback=map';
   document.body.appendChild(script);
-}
+};
 
-window.onload = loadScript;
+var text = ["writing code!", "designing websites!", "testing!", "challanges!"];
+var count = 0;
+
+function textAnimation(){
+	
+	var counter = 0;
+	var test = document.getElementById('mod');
+
+	
+	var x = setInterval(function(){
+		test.innerHTML += text[count][counter];		
+		counter++;		
+		if(counter===text[count].length){
+			clearInterval(x);
+			setTimeout(function(){
+				var y = setInterval(function(){
+					var hello =  test.innerHTML;
+					var helloCut = hello.slice(0, counter);
+					test.innerHTML = helloCut;
+					counter--;
+					if(counter===0){
+						setTimeout(function(){
+							test.innerHTML = "";
+						},150);						
+						setTimeout(function(){
+							clearInterval(y);
+							if(count +1 === text.length){
+								count = -1;
+							};
+							textAnimation(count++);
+						}, 1000);						
+					}
+				},100);
+			},2000);
+		};	
+	},150);	
+	
+
+};
+function dash(){
+	var dash = document.getElementById("dash");
+	var hide = setInterval(function(){
+		if(dash.style.opacity === "1"){
+			dash.style.opacity = "0";
+		}
+		else {
+			dash.style.opacity = "1";
+		}
+	}, 600);
+};
+
+window.onload = function() {
+
+loadScript();
+textAnimation(count);
+dash();
+};
